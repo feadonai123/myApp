85abdd972a1cb7bdb8a45bb68353a7e51817fb1b
feadonai123 <felipe.2002@alunos.utfpr.edu.br>
2021-06-29 00:34:39 -0300

    add 'src' folder

---
+++ myApp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default App = ()=>{
+    return(
+        <View style={{flex:1, backgroundColor: '#00f'}}>
+
+        </View>
+    );
+}
